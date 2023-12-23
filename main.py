80c337bb26ade0e918fa4be059e54fcc77fb1d8c
Ankesh Kumar <60851047+notelyfe@users.noreply.github.com>
2023-12-23 18:38:53 +0530

    added a pause option

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from tkinter import *
 from playsound import playsound
 from tkinter import messagebox
 import time
+import os
 
 root = Tk()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class Timer:
     def __init__(self, root):
@@ -56,16 +67,16 @@ class Timer:
         timerFrame = Frame(mainFrame, bg = "#000", pady=10)
         timerFrame.grid(row=1, column=0)
 
-        btnFrame = Frame(mainFrame, bg = "#000", pady=10)
-        btnFrame.grid(row=2, column=0)
+        self.btnFrame = Frame(mainFrame, bg = "#000", pady=10)
+        self.btnFrame.grid(row=2, column=0)
 
         self.timer = Label(timerFrame, text="00:00:00", font="arial 16 bold", bg="#000", fg="white", justify=CENTER, width=15)
         self.timer.grid(row=0, column=0)
 
-        self.btnStart = Button(btnFrame, text="Start", font="arial 16 bold", bg="green", fg="white", command=self.Start)
+        self.btnStart = Button(self.btnFrame, text="Start", font="arial 14 bold", bg="green", fg="white", command=self.Start)
         self.btnStart.grid(row=0, column=0)
 
-        self.btnStop = Button(btnFrame, text="Stop", font="arial 16 bold", bg="red", fg="white", command=self.Stop)
+        self.btnStop = Button(self.btnFrame, text="Stop", font="arial 14 bold", bg="red", fg="white", command=self.Stop)
         self.btnStop.grid(row=0, column=1)
 
     def Start(self):
@@ -82,34 +93,38 @@ class Timer:
             hours = 0
 
         if int(seconds) >= 60:
-            playsound("error.mp3")
+            playsound(resource_path("error.mp3"))
             messagebox.showerror("Error","Error!! Can't set seconds greater than 59.")
             self.secVal.set("")
 
         elif int(minutes) >= 60:
-            playsound("error.mp3")
+            playsound(resource_path("error.mp3"))
             messagebox.showerror("Error","Error!! Can't set Minutes greater than 59.")
             self.minVal.set("")
 
         elif int(hours) >= 25:
-            playsound("error.mp3")
+            playsound(resource_path("error.mp3"))
             messagebox.showerror("Error","Error!! Can't set hours greater than 24.")
             self.hrsVal.set("")
         
         else:
+
+            self.btnStart.config(text="Pause", command=self.pause, bg="yellow", fg="black")
+
             self.running = True
             self.secVal.set("")
             self.minVal.set("")
             self.hrsVal.set("")
-            self.update_timer(int(hours), int(minutes), int(seconds) )
+            self.time = int(hours)*3600 + int(minutes)*60 + int(seconds)
+            self.update_timer()
 
     def Stop(self):
         self.running = False
         self.timer.config(text="00:00:00")
 
-    def update_timer(self, hours, minutes, seconds):
+    def update_timer(self):
 
-        self.time = hours * 3600 + minutes * 60 + seconds    # time in seconds
+        self.running = True
 
         while self.time > -1 and self.running == True:
 
@@ -122,10 +137,16 @@ class Timer:
             time.sleep(1)
 
             if self.time == 0:
-                playsound("alarm.mp3")
+                playsound(resource_path("alarm.mp3"))
                 messagebox.showinfo("Info","Times up...")
+                self.btnStart.config(text="Start", command=self.Start, bg="green", fg="white")
+
 
             self.time -= 1
+
+    def pause(self):
+        self.btnStart.config(text="Start", command=self.update_timer, bg="green", fg="white")
+        self.running = False
 
 timer = Timer(root)
 root.mainloop()
